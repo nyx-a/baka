3ccfff3ede8bdc4ff92d03321b96b4d4e173a6ee
nyx-a <nyx-a@live.jp>
2021-04-12 16:10:10 +0900

    doctor()を追加。メソッドを一部整理。

--- blob-apartment.rb
+++ blob-apartment.rb
@@ -11,6 +11,21 @@ class BlobApartment
   #
 
   class << self
+    def make_instance(
+      collection:, server:, database:, user:, password:, auth:nil
+    )
+      new(
+        client: Mongo::Client.new(
+          [server],
+          database:    database,
+          user:        user,
+          password:    password,
+          auth_source: auth,
+        ),
+        collection: collection
+      )
+    end
+
     def mimetype blob
       FileMagic.fm(FileMagic::MAGIC_MIME_TYPE).buffer blob
     end
@@ -55,28 +70,33 @@ class BlobApartment
 
   def initialize client:, collection:
     @client = client
-    @possession = @client[collection + '.digests']
+    @possession = @client[collection + '.possessions']
     @bucket = Mongo::Grid::FSBucket.new @client, fs_name:collection
   end
 
   #
   #* upload / download
   #
+  # 全く同じデータをほぼ同時にuploadした場合重複が起こり得る
+  # 全く同じデータであるならば重複しても問題はない
+  #
 
+  # BSON::Binary(sha256) ... first insert
+  # nil                  ... already has that blob
   def upload blob
-    sha256_s = BlobApartment.sha256 blob
-    sha256_b = BlobApartment.string_to_bson sha256_s
-    mimetype = BlobApartment.mimetype blob
-    if register sha256_b
+    sha256str  = BlobApartment.sha256 blob
+    sha256bson = BlobApartment.string_to_bson sha256str
+    mimetype   = BlobApartment.mimetype blob
+    if register sha256bson
       @bucket.upload_from_stream(
-        sha256_s,
+        sha256str,
         blob,
         metadata:{
-          sha256:   sha256_b,
+          sha256:   sha256bson,
           mimetype: mimetype,
         }
       )
-      return sha256_b
+      return sha256bson
     end
   end
 
@@ -86,25 +106,25 @@ class BlobApartment
     end
   end
 
-  def download digest, to:nil
-    doc = find_doc_with_digest digest
+  def download target, to=nil
+    doc = sole_completion target
     if doc
       to = '' if to.nil?
-      @bucket.download_to_stream doc[:_id], to
+      @bucket.download_to_stream doc[:id], to
       return to
     end
   end
 
-  def download_into_file digest, path:nil
-    doc = find_doc_with_digest digest
+  def download_to_file target, path=nil
+    doc = sole_completion target
     if doc
       if path.nil?
-        f = doc[:filename]
-        s = BlobApartment.suffix_for doc[:metadata][:mimetype]
-        path = "#{f}.#{s}"
+        path = BlobApartment.bson_to_string doc[:sha256]
+        sfx  = BlobApartment.suffix_for doc[:mimetype]
+        path += ".#{sfx}" if sfx
       end
       open(path, 'wb') do |fout|
-        @bucket.download_to_stream doc[:_id], fout
+        @bucket.download_to_stream doc[:id], fout
       end
       return true
     end
@@ -114,19 +134,30 @@ class BlobApartment
   #* find
   #
 
-  def find_docs(...)
-    @bucket.find(...).map{ BlobApartment.bson_to_h _1 }
+  def list mimetype:nil, digest:nil, limit:nil
+    if digest.is_a? String
+      digest = BlobApartment.string_to_bson digest
+    end
+    query  = { }
+    option = { }
+    query.update 'metadata.mimetype':mimetype if mimetype
+    query.update 'metadata.sha256':digest if digest
+    option.update 'limit':limit if limit
+    @bucket.find(query, option).map{ BlobApartment.bson_to_h _1 }
   end
 
-  def find_docs_with_type mimetype
-    @bucket
-      .find('metadata.mimetype':mimetype)
-      .map{ BlobApartment.bson_to_h _1 }
-  end
-
-  def find_doc_with_digest digest
-    doc = @bucket.find({'metadata.sha256':digest}, {limit:1}).first
-    BlobApartment.bson_to_h doc
+  def sole_completion target
+    case target
+    when BSON::ObjectId
+      doc = @bucket.find({_id:target}, {limit:1}).first
+      if doc
+        BlobApartment.bson_to_h doc
+      end
+    when BSON::Binary, String
+      list(digest:target, limit:1)&.first
+    else
+      raise TypeError, "invalid class for target => #{target.class}"
+    end
   end
 
   def digest? digest
@@ -135,6 +166,44 @@ class BlobApartment
 
   def data? data
     digest? BlobApartment.sha256_in_bson data
+  end
+
+  def has_file? path
+    buffer     = open(path, 'rb').read
+    length     = buffer.length
+    sha256bson = BlobApartment.sha256_in_bson buffer
+    mimetype   = BlobApartment.mimetype buffer
+    d = sole_completion sha256bson
+    (d and d[:length]==length and d[:mimetype]==mimetype)
+  end
+
+  #
+  #* consistency check
+  #
+
+  def doctor
+    count = 0
+    for i in @bucket.find
+      sha256bson = i[:metadata][:sha256]
+      buffer = ''
+      @bucket.download_to_stream i[:_id], buffer
+      twice = BlobApartment.sha256_in_bson buffer
+
+      if i[:length] != buffer.length
+        puts "ERROR: Inconsistencies in size"
+        p i
+      end
+      if sha256bson != twice
+        puts "ERROR: Inconsistencies in digest"
+        p i
+      end
+      if BlobApartment.bson_to_string(twice) != i[:filename]
+        puts "ERROR: Inconsistencies in filename"
+        p i
+      end
+      count += 1
+    end
+    count
   end
 
   private
